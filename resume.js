0b4928282fcfc7ba2629378be476ddc3550c43d2
Asvin-Prabakaran <157348152+Asvin-Prabakaran@users.noreply.github.com>
2024-02-01 20:36:44 +0530

    resume with for in loop
    day05

---
+++ resume.js
@@ -0,0 +1,17 @@
+var personal = [{
+  "id":"asvin","age":22,
+  "degree":"btech",
+  "Skills":"c programming",
+  "Address":"no 5 south street pondicherry",
+  "Phone":"638260XXX",
+  "parents":"prabakaran"}];
+  for(var i in personal){
+    console.log(personal[i].id);
+    console.log(personal[i].age);
+    console.log(personal[i].degree);
+    console.log(personal[i].Skills);
+    console.log(personal[i].Address);
+    console.log(personal[i].Phone);
+    console.log(personal[i].parents);
+    
+  }
